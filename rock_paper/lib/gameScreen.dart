3d522fa0a6fe4cfb71175f6e7629b3b198957002
Nacuta Claudiu <nacutac@gmail.com>
2020-06-03 22:28:04 +0300

    Winner image

--- rock_paper/lib/gameScreen.dart
+++ rock_paper/lib/gameScreen.dart
@@ -5,9 +5,9 @@ import 'package:rockpaper/main.dart';
 import 'package:rockpaper/winnerLogic.dart';
 
 class GameScreen extends StatefulWidget {
-  String userImage;
+  dynamic userImage;
   int userChoice;
-  GameScreen(this.userImage, this.userChoice);
+  GameScreen(this.userImage);
   CpuChoice cpuChoice = CpuChoice();
 
   @override
@@ -20,7 +20,7 @@ class GameScreenState extends State<GameScreen> {
   Test test = Test();
 
   Widget userWidget() {
-    String image = widget.userImage;
+    String image = test.images[widget.userImage];
     return Container(
       child: FlatButton(
         // color: color,
@@ -42,32 +42,10 @@ class GameScreenState extends State<GameScreen> {
     );
   }
 
-  Widget cpuWidget() {
-    String image = test.images[cpuChoice.cpuRandomNumber];
-    return Container(
-      child: FlatButton(
-        // color: color,
-        onPressed: () {
-          setState(() {});
-        },
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              Image.asset(
-                "assets/$image.png",
-                width: 180,
-                height: 180,
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+ 
 
   String text;
   Widget won() {
-    writeWinner();
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Align(
@@ -84,23 +62,7 @@ class GameScreenState extends State<GameScreen> {
     );
   }
 
-  String writeWinner() {
-    if ((widget.userChoice == 0 && cpuChoice.cpuRandomNumber == 0) ||
-        (widget.userChoice == 1 && cpuChoice.cpuRandomNumber == 1) ||
-        (widget.userChoice == 2 && cpuChoice.cpuRandomNumber == 2)) {
-      return text = "You manage a Draw game, pathetic.";
-    } else if ((widget.userChoice == 0 && cpuChoice.cpuRandomNumber == 1) ||
-        (widget.userChoice == 1 && cpuChoice.cpuRandomNumber == 2) ||
-        (widget.userChoice == 2 && cpuChoice.cpuRandomNumber == 0)) {
-      // cpuScore++;
-      return text = "I know you're better than that, but this time You Lose !!";
-    } else if ((widget.userChoice == 0 && cpuChoice.cpuRandomNumber == 2) ||
-        (widget.userChoice == 1 && cpuChoice.cpuRandomNumber == 0) ||
-        (widget.userChoice == 2 && cpuChoice.cpuRandomNumber == 1)) {
-      // userScore++;
-      return text = "Finally the target it's reached, You Won !!";
-    }
-  }
+
 
   Widget verticalLayout() {
     return SafeArea(
@@ -112,22 +74,22 @@ class GameScreenState extends State<GameScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             userWidget(),
-            Center(
-              child: Text(
-                "VS",
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.blue,
-                  fontWeight: FontWeight.w900,
-                ),
-              ),
-            ),
-            cpuWidget(),
+            // Center(
+            //   child: Text(
+            //     "VS",
+            //     style: TextStyle(
+            //       fontSize: 24,
+            //       color: Colors.blue,
+            //       fontWeight: FontWeight.w900,
+            //     ),
+            //   ),
+            // ),
+            // cpuWidget(),
 
             // Container(
             //   child: Text("Ce ai facut> $text"),
             // ),
-            Container(child: won()),
+            // Container(child: won()),
             Container(
               child: RaisedButton(
                 color: Colors.greenAccent,
@@ -167,17 +129,17 @@ class GameScreenState extends State<GameScreen> {
             Row(
               children: <Widget>[
                 userWidget(),
-                Center(
-                  child: Text(
-                    "VS",
-                    style: TextStyle(
-                      fontSize: 24,
-                      color: Colors.blue,
-                      fontWeight: FontWeight.w900,
-                    ),
-                  ),
-                ),
-                cpuWidget(),
+                // Center(
+                //   child: Text(
+                //     "VS",
+                //     style: TextStyle(
+                //       fontSize: 24,
+                //       color: Colors.blue,
+                //       fontWeight: FontWeight.w900,
+                //     ),
+                //   ),
+                // ),
+                // cpuWidget(),
                 Column(
                   children: [
                     Container(width: 250, child: won()),
